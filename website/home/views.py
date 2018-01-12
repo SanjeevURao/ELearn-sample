08ff814cbc471492a487286ed8f72e3cf83e5d1d
Sanjeev U Rao <sanjeevurao@gmail.com>
2018-01-12 16:48:51 +0530

    Add linke to external websites

--- website/home/views.py
+++ website/home/views.py
@@ -6,6 +6,4 @@ from django.shortcuts import render
 
 def index(request):
     template = loader.get_template('home/index.html')
-    a=[1,2,3,4]
-    context={'a': a, }
-    return render(request,'home/index.html',context)
+    return render(request,'home/index.html')
