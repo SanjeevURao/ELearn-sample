aeb523b961ee87ee4ef3d947756ed6879ae5d4d8
Sanjeev U Rao <sanjeevurao@gmail.com>
2018-01-12 11:33:25 +0530

    Add  static files

--- website/home/views.py
+++ website/home/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
     template = loader.get_template('home/index.html')
-    a=[1,2,3]
-    context={
-        'a': a,
-    }
-    return HttpResponse(template.render(context,request))
+    a=[1,2,3,4]
+    context={'a': a, }
+    return render(request,'home/index.html',context)
